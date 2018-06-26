be09bc8dc23cd9b1310385b5bb48dca0b77f55d3
dhrbn <davidharbine@gmail.com>
2018-06-26 14:39:50 +0200

    Reordered FSL's processes and the way to handle the matlab command for SPM standalone

---
+++ python/populse_mia/src/modules/PipelineManager/Processes/MIA_processes/FSL/processes.py
@@ -0,0 +1,45 @@
+# Capsul import
+from capsul.api import Process
+from capsul.api import StudyConfig, get_process_instance
+
+# Trait import
+from traits.api import Float
+from nipype.interfaces.base import File
+
+# Other import
+import os
+from nipype.interfaces import fsl
+
+
+class Smooth(Process):
+
+    def __init__(self):
+        super(Smooth, self).__init__()
+
+        self.add_trait("in_file", File(output=False))
+        self.add_trait("fwhm", Float(output=False))
+        self.add_trait("out_file", File(output=True))
+
+    def _run_process(self):
+
+        smooth_process = get_process_instance(fsl.Smooth)
+        smooth_process.in_file = self.in_file
+        smooth_process.out_file = self.out_file
+        smooth_process.output_type = 'NIFTI'
+
+
+        if self.fwhm > 0:
+            smooth_process.fwhm = self.fwhm
+        else:
+            smooth_process.fwhm = 2.0
+
+        smooth_process.output_directory = os.path.split(self.out_file)[0]
+        smooth_process.run()
+
+        import nibabel as nib
+        img = nib.load(self.out_file + ".gz")
+        nib.save(img, self.out_file)
+
+        # Display
+        print('Smoothing with FSL\n...\nInputs: {', self.in_file, ', ',
+              self.fwhm, '}\nOutput: ', self.out_file, '\n...\n')
