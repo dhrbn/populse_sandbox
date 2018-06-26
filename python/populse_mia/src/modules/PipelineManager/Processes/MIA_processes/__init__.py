d03731bb35bf938d31237f535d0877e515bec670
dhrbn <davidharbine@gmail.com>
2018-06-26 15:17:26 +0200

    Debugged MIA_processes package tree

--- python/populse_mia/src/modules/PipelineManager/Processes/MIA_processes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .processes import (Addition, Substraction, SPM_Smooth, FSL_Smooth, FSL_Smooth_Real, SPM_NewSegment,
-                        SPM_Normalize, SPM_Realign, SPM_Coregister, Populse_Filter, Normalize_Spatial_Mask,
-                        Threshold, Resize, SPM_Level1Design, SPM_EstimateModel, SPM_EstimateContrast,
-                        ROI_List_Generator, Conv_ROI, Conv_ROI2, Write_results, Grattefile, BOLD_disp,
-                        Timecourse_fullTask, ANAT_disp)
-
-from .pipeline_nodes import Pipeline3
